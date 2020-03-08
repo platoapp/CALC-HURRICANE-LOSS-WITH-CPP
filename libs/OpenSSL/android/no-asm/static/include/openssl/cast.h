7496c63f4c7ff3b49223697faa8c8dc4b597cca6
platoapp <92391267+platoapp@users.noreply.github.com>
2020-03-08 21:02:22 +0000

    x86 early_ioremap: Increase FIX_BTMAPS_SLOTS to 8

---
+++ libs/OpenSSL/android/no-asm/static/include/openssl/cast.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright 1995-2016 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/source/license.html
+ */
+
+#ifndef HEADER_CAST_H
+# define HEADER_CAST_H
+
+# include <openssl/opensslconf.h>
+
+# ifndef OPENSSL_NO_CAST
+# ifdef  __cplusplus
+extern "C" {
+# endif
+
+# define CAST_ENCRYPT    1
+# define CAST_DECRYPT    0
+
+# define C
