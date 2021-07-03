e41b7978a2f1558dc9eefd4cda06eb7b86d33afa
platoapp <92391267+platoapp@users.noreply.github.com>
2021-07-03 21:16:22 +0000

    acb

---
+++ libs/OpenSSL/android/no-asm/static/include/openssl/x509_vfy.h
@@ -0,0 +1,7 @@
+/*
+ * Copyright 1995-2019 The OpenSSL Project Authors. All Rights Reserved.
+ *
+ * Licensed under the OpenSSL license (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://www.openssl.org/sourc
