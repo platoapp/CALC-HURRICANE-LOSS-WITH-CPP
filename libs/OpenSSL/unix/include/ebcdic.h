f70ac80f855f2187bfc5fc2209933612774cbf1a
platoapp <92391267+platoapp@users.noreply.github.com>
2021-12-04 06:57:57 +0000

    * Doh.

--- libs/OpenSSL/unix/include/ebcdic.h
+++ libs/OpenSSL/unix/include/ebcdic.h
@@ -25,4 +25,9 @@ extern "C" {
 extern const unsigned char os_toascii[256];
 extern const unsigned char os_toebcdic[256];
 void *ebcdic2ascii(void *dest, const void *srce, size_t count);
-void *ascii2ebcdic(void *dest, const void *srce, size_t cou
+void *ascii2ebcdic(void *dest, const void *srce, size_t count);
+
+#ifdef  __cplusplus
+}
+#endif
+#endif
